73b6601225b6d348b377d8eb99896f79d6507734
YardenShoham <hrsi88@gmail.com>
2019-10-07 19:59:39 +0300

    Updates

---
+++ precache-manifest.fea1f2ed37ad53418238acf40e186676.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2acafba43b8b98eaff5f8f2de6642ba",
+    "url": "/coin-trend-notifier-website/index.html"
+  },
+  {
+    "revision": "2bf0ff77bffbf0201d24",
+    "url": "/coin-trend-notifier-website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "03ad9ad001cf65d06f01",
+    "url": "/coin-trend-notifier-website/static/js/2.417857c8.chunk.js"
+  },
+  {
+    "revision": "2bf0ff77bffbf0201d24",
+    "url": "/coin-trend-notifier-website/static/js/main.6fb53433.chunk.js"
+  },
+  {
+    "revision": "3ea299a5ce5a460182e3",
+    "url": "/coin-trend-notifier-website/static/js/runtime-main.c023e4a3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/coin-trend-notifier-website/static/media/logo.25bf045c.svg"
+  }
+]);
