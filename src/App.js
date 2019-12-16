92bab1621153138428c923e2cd4c57e42dd2d95b
Aviel212 <Aviel9731@gmail.com>
2019-12-16 21:54:11 +0200

    Add React-bootstrap
    added react-bootstrap and bootstrap to the packages.
    added an idea to a navigation bar for the main Layout of the system.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+// import "./App.css";
+import { Navbar, Nav, Form, Button, FormControl } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="NotRegister">
+      <Navbar bg="dark" variant="dark" fixed="top">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+        </Nav>
+        <Nav className="mr-sm-2" variant="dark">
+          <Nav.Link href="#account">Account</Nav.Link>
+        </Nav>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <div className="App">
+      
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,8 +39,5 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div> */
 }
-
-export default App;
