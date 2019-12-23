1dfe192322f4f1d5e0067162fb5c1870e029f7c9
Aviel212 <Aviel9731@gmail.com>
2019-12-23 13:42:42 +0200

    :green_heart: Remove unnecessary imports, App is now a Class.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import React from "react";
-import logo from "./logo.svg";
-// import "./App.css";
-import { Navbar, Nav, Form, Button, FormControl } from "react-bootstrap";
+import React, { Component } from "react";
+import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="NotRegister">
-      <Navbar bg="dark" variant="dark" fixed="top">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-        </Nav>
-        <Nav className="mr-sm-2" variant="dark">
-          <Nav.Link href="#account">Account</Nav.Link>
-        </Nav>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="NotRegister">
+        <Navbar bg="dark" variant="dark" fixed="top">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+          </Nav>
+          <Nav className="mr-sm-2" variant="dark">
+            <Nav.Link href="#account">Account</Nav.Link>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default App;
-
-{
-  /* <div className="App">
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div> */
-}
