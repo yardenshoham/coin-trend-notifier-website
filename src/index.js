66da577e54472e854dbe5436848f4064bb68c525
Aviel212 <Aviel9731@gmail.com>
2019-12-23 14:28:05 +0200

    :white_check_mark:  Update index.js, ignore the lines while testing.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-/* istanbul ignore next file */
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-
+/* istanbul ignore next */
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+/* istanbul ignore next */
 serviceWorker.unregister();
