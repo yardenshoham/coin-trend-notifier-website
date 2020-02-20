f84ab43f8128e0d69d1487100c600351d88103d9
Aviel212 <Aviel9731@gmail.com>
2020-02-20 14:59:18 +0200

    logo and responsive Navbar
    Added logo and responsive Navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ export default class App extends Component {
     return (
       <div className="NotRegister">
         <HomepageNavbar />
-
         <div
           style={{
             position: "relative",
--- src/components/HomepageNavbar.jsx
+++ src/components/HomepageNavbar.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
 
 const HomepageNavbar = () => {
   return (
     <React.Fragment>
-      <Navbar bg="dark" variant="dark">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-        </Nav>
-        <Nav className="mr-sm-2" variant="dark">
-          <Nav.Link href="#account">Account</Nav.Link>
-        </Nav>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            width="80"
+            className="d-inline-block align-top"
+            alt="logo"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+            </Nav>
+          </Nav>
+          <Nav className="mr-sm-2" variant="dark">
+            <Nav.Link href="#account">Account</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </React.Fragment>
   );
