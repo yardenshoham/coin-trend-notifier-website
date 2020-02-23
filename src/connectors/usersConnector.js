b7ab34f23c9ff391030319f0c23e00fe521ff962
Aviel212 <Aviel9731@gmail.com>
2020-02-23 12:49:31 +0200

    Finished usersConnector

--- src/connectors/usersConnector.js
+++ src/connectors/usersConnector.js
@@ -5,131 +5,61 @@ const path = "https://coin-trend-notifier-api.herokuapp.com/";
 const log = console.log;
 
 // Checks the server's availablity
-const check_servers_availablity = () => {
-  axios
-    .get(path)
-    .then(function(res) {
-      log(res.data);
-    })
-    .catch(function(err) {
-      log(err);
-    });
-};
-
-let res;
-const create_new_user = func => {
-  axios
-    .post(path + "api/users", {
-      email: "test.av3@gmail.com",
-      password: "12345678",
-      username: "test_user3",
-      alertLimit: 0
-    })
-    .then(function(res) {
-      res = res.data;
-    })
-    .catch(function(err) {
-      log(err);
-    })
-    .then(function() {
-      func();
-    });
-};
-create_new_user(() => {
-  log(res);
-});
-
-// Creates a new user
-const create = () => {
-  req.post(
-    {
-      headers: {},
-      url: path + "users",
-      json: true,
-      body: JSON.stringify()
-    },
-    (err, res, body) => {
-      if (err) {
-        log(err);
-      } else if (res.statusCode === 422) {
-        log("User already exist");
-      } else {
-        log(body);
-      }
-    }
-  );
-};
-
-/*
-// Update a user
-req.put(
-  {
-    headers: {},
-    url: path + "users",
-    json: true,
-    body: JSON.stringify({
-      email: "test.me@gmail.com",
-      username: "test_user",
-      phoneNumber: "+972-523546888",
-      alertLimit: 604800
-    })
-  },
-  (err, res, body) => {
-    if (err) {
-      log(err);
-    } else if (res.statusCode === 400) {
-      log("Validation Error");
-    } else if (res.statusCode === 409) {
-      log("Email is taken");
-    } else {
-      log(body);
-    }
+const check_servers_availablity = async () => {
+  try {
+    log(await axios.get(path).data);
+  } catch (err) {
+    log(err);
   }
-);
+};
+
+const createNewUser = async (
+  email,
+  password,
+  username,
+  phoneNumber,
+  alertLimit
+) => {
+  return (
+    await axios.post(path + "api/users", {
+      email,
+      password,
+      username,
+      phoneNumber,
+      alertLimit
+    })
+  ).data;
+};
+
+// Update a user
+const updateUser = async (
+  email,
+  password,
+  username,
+  phoneNumber,
+  alertLimit
+) => {
+  return await axios.put(path + "api/users", {
+    email,
+    password,
+    username,
+    phoneNumber,
+    alertLimit
+  }).data;
+};
 
 // Login a user
-req.post(
-  {
-    headers: {},
-    url: path + "users",
-    json: true,
-    body: JSON.stringify({
-      email: "test.user@testdomain.com",
-      password: "123456789"
-    })
-  },
-  (err, res, body) => {
-    if (err) {
-      log(err);
-    } else if (res.statusCode === 401) {
-      log("User unregistered or Incorrect password");
-    } else {
-      log(body);
-    }
-  }
-);
+const loginUser = async (email, password) => {
+  const res = await axios.post(path + "users/login", { email, password });
+  localStorage.setItem("jwt", res.data.jwt);
+};
 
-// change a user's password
-req.patch(
-  {
-    headers: {},
-    url: path + "users/password",
-    json: true,
-    body: JSON.stringify({
-      oldPassword: "12345678",
-      newPassword: "123456789"
-    })
-  },
-  (err, res, body) => {
-    if (err) {
-      log(err);
-    } else if (res.statusCode === 400) {
-      log("Validation Error");
-    } else if (res.statusCode === 422) {
-      log("User not exist or Wrong password");
-    } else {
-      log(body);
-    }
-  }
-);
-*/
+// Change user password
+const changeUserPassword = async (oldPassword, newPassword) => {
+  let config = { headers: { Authorization: `Bearer ${token}` } };
+  return await axios.patch(path + "users/password", {
+    oldPassword,
+    newPassword,
+    config
+  });
+};
