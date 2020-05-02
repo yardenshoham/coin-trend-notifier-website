bde5260d3fac3f8ca917e3aa06343221a0cb0f47
Aviel212 <Aviel9731@gmail.com>
2020-05-02 14:35:43 +0300

    Fix issue #38
    A possible fix for the issue #38

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 export default function Home(props) {
   const history = useHistory();
   useEffect(() => {
+    console.log("history is: " + props.history);
     if (localStorage.getItem("jwt")) {
       history.push("/trends");
     } else {
