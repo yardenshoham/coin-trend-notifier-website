a9f1649c052ea91e273ea4909b1e6c2c16c4528c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-25 19:12:36 +0000

    Deploy yardenshoham/coin-trend-notifier-website to github.com/yardenshoham/coin-trend-notifier-website.git:gh-pages

---
+++ precache-manifest.410ed5870138f8d854bb6f2435ce2154.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eb1c0c5c726564af684dfa3787fe8a",
+    "url": "/coin-trend-notifier-website/index.html"
+  },
+  {
+    "revision": "182c13c432a476c1e5ed",
+    "url": "/coin-trend-notifier-website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a33035d9fe35bc26f220",
+    "url": "/coin-trend-notifier-website/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "182c13c432a476c1e5ed",
+    "url": "/coin-trend-notifier-website/static/js/2.ec5aa5fb.chunk.js"
+  },
+  {
+    "revision": "0cb50bd4c306edc174ecc490959eeccc",
+    "url": "/coin-trend-notifier-website/static/js/2.ec5aa5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33035d9fe35bc26f220",
+    "url": "/coin-trend-notifier-website/static/js/main.cc8e831b.chunk.js"
+  },
+  {
+    "revision": "5f5aaeb0a123364d348e",
+    "url": "/coin-trend-notifier-website/static/js/runtime-main.39794221.js"
+  },
+  {
+    "revision": "6d974881a5511ba11ca7b65af4fb94e4",
+    "url": "/coin-trend-notifier-website/static/media/logo.6d974881.png"
+  }
+]);
