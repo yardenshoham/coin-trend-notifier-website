36be1738c23d09384667b7d4cc8ac495c5c1f072
Aviel212 <Aviel9731@gmail.com>
2020-06-05 13:32:09 +0300

    Edit event page

--- src/components/Trends/Trend.jsx
+++ src/components/Trends/Trend.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Container, Row, Col, Badge, Jumbotron } from "react-bootstrap";
 import * as eventConnector from "../../connectors/eventConnector";
 
 export default function Trend(props) {
@@ -7,6 +8,21 @@ export default function Trend(props) {
   const [firedAt, setFiredAt] = useState("");
   const [baseAsset, setBaseAsset] = useState("");
   const [quoteAsset, setQuoteAsset] = useState("");
+
+  const msToDateTime = (ms) => {
+    let dateTime = new Date(ms);
+    return {
+      date: `${dateTime.getDay()}/${dateTime.getMonth()}/${dateTime.getFullYear()}`,
+      time: `${dateTime.getHours()}:${dateTime.getMinutes()}:${dateTime.getSeconds()}`,
+    };
+  };
+
+  const probToProb = (prob) => {
+    return {
+      type: prob < 0 ? 0 : 1,
+      probability: (Math.abs(prob) * 100).toPrecision(4) + "%",
+    };
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -23,12 +39,77 @@ export default function Trend(props) {
 
   return (
     <React.Fragment>
-      <div>
-        <h1>ID is: {props.match.params.id}</h1>
-        <h1>Probability is: {probability}</h1>
-        <h1>Timestamp is: {firedAt}</h1>
-        <h1>Asset: {baseAsset + "/" + quoteAsset}</h1>
-      </div>
+      <Container
+        fluid
+        className="HomepageBody"
+        style={{
+          border: "1px solid gray",
+          padding: "50px",
+          position: "relative",
+          top: "10%",
+          margin: "auto",
+          boxShadow: "0px 0px 8px -2px",
+          borderRadius: "1%",
+          width: "80%",
+        }}
+      >
+        <Row style={{ textAlign: "center" }}>
+          <Col>
+            <Jumbotron style={{ padding: "5%" }}>
+              <h1>
+                ID: <Badge variant="secondary">{props.match.params.id}</Badge>
+              </h1>
+            </Jumbotron>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Asset:</h1>
+          </Col>
+          <Col>
+            <h1>
+              <Badge variant="secondary">{baseAsset + "/" + quoteAsset}</Badge>
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Probability:</h1>
+          </Col>
+          <Col>
+            <h1>
+              <Badge
+                variant={
+                  probToProb(probability).type === 0 ? "danger" : "success"
+                }
+              >
+                {probToProb(probability).probability}
+              </Badge>
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Date:</h1>
+          </Col>
+          <Col>
+            <h1>
+              <Badge variant="secondary">{msToDateTime(firedAt).date}</Badge>
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h1>Time:</h1>
+          </Col>
+          <Col>
+            <h1>
+              <Badge variant="secondary">{msToDateTime(firedAt).time}</Badge>
+            </h1>
+          </Col>
+        </Row>
+        {/* maybe add here the value when fired and the current value */}
+      </Container>
     </React.Fragment>
   );
 }
